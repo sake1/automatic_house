38845231ab4d369f88dc77845a9034cdeabf91c6
sake1 <timothydickys@gmail.com>
2017-11-13 14:23:19 +0700

    motii

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -25,8 +25,14 @@ import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import abstracts.AbstractActor;
+import abstracts.AbstractSensor;
+import actors.AirConditioner;
+import actors.Window;
+import interfaces.Actor;
 import interfaces.Sensor;
 import sensors.Anemometer;
+import sensors.Clock;
 import sensors.Thermometer;
 
 public class GUI extends JFrame {
@@ -65,166 +71,160 @@ public class GUI extends JFrame {
     }
     
     public void addSensor(Sensor s) {
-    	
+    	if(s instanceof Clock) {
+    		addClock(s);
+    	} else if(s instanceof Anemometer) {
+    		addAnemometer(s);
+    	} else if(s instanceof Thermometer) {
+    		addThermometer(s);
+    	}
     }
      
-    public void addComponentsToPane(final Container pane) {
-//        /**First Row**/ 
-//        //Add buttons to experiment with Grid Layout
-//        JLabel sensorLabel = new JLabel("Sensor");
-//        sensorLabel.setFont(new Font("Serif", Font.BOLD, 17));
-//        sensorLabel.setVerticalAlignment(SwingConstants.CENTER);
-//        sensorLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//        panel.add(sensorLabel);
-//        
-//        panel.add(new JLabel(""));
-//        panel.add(new JLabel(""));
-//        
-//        JLabel actorLabel = new JLabel("Actor");
-//        actorLabel.setFont(new Font("Serif", Font.BOLD, 17));
-//        actorLabel.setVerticalAlignment(SwingConstants.CENTER);
-//        actorLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//        panel.add(actorLabel);
-//        
-//        panel.add(new JLabel(""));
-//        /**First Row**/ 
-//        
-//        /**Second Row**/ 
-//        JLabel thermometerLabel = new JLabel("Temperature " + TEMPERATURE_INIT);
-//        thermometerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//        panel.add(thermometerLabel);
-//        
-//        JSlider temperatureSlider = new JSlider(JSlider.HORIZONTAL,
-//                Thermometer.MIN_VALUE, Thermometer.MAX_VALUE, TEMPERATURE_INIT);
-//
-//        temperatureSlider.setMajorTickSpacing(75);
-//        temperatureSlider.setPaintTicks(true);
-//        temperatureSlider.setPaintLabels(true);
-//        temperatureSlider.addChangeListener(new ThermometerListener(thermometerLabel));
-//        panel.add(temperatureSlider);
-//        panel.add(new JLabel(""));
-//        
-//        JLabel airConditionerLabel = new JLabel("Air Conditioner");
-//        airConditionerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//        panel.add(airConditionerLabel);
-//        
-//        JSlider airConditionerSlider = new JSlider(JSlider.HORIZONTAL,
-//                0, 2, 1);
-//        airConditionerSlider.setPaintLabels(true);
-//        airConditionerSlider.setPaintTicks(true);
-//        airConditionerSlider.setMajorTickSpacing(3);
-//        airConditionerSlider.setMinorTickSpacing(1);
-//        
-//        Dictionary<Integer, Component> airConditionerLabelTable = new Hashtable<Integer, Component>();
-//        airConditionerLabelTable.put(0, new JLabel("HEATER"));
-//        airConditionerLabelTable.put(1, new JLabel("COOLER"));
-//        airConditionerLabelTable.put(2, new JLabel("OFF"));
-//   
-//        airConditionerSlider.setLabelTable(airConditionerLabelTable);
-//        airConditionerSlider.setEnabled(false);
-//        panel.add(airConditionerSlider);
-//        /**Second Row**/ 
-//        
-//        /**Third Row**/ 
-//        JLabel anemometerLabel = new JLabel("Wind Speed " + WINDSPEED_INIT);
-//        anemometerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//        panel.add(anemometerLabel);
-//        
-//        JSlider anemometerSlider = new JSlider(JSlider.HORIZONTAL,
-//                Anemometer.MIN_VALUE, Anemometer.MAX_VALUE, WINDSPEED_INIT);
-//
-//        anemometerSlider.setMajorTickSpacing(2);
-//        anemometerSlider.setPaintTicks(true);
-//        anemometerSlider.setPaintLabels(true);
-//        anemometerSlider.addChangeListener(new AnemometerListener(anemometerLabel));
-//        panel.add(anemometerSlider);
-//        panel.add(new JLabel(""));
-//        
-//        JLabel windowLabel = new JLabel("Window");
-//        windowLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//        panel.add(windowLabel);
-//        
-//        JSlider windowSlider = new JSlider(JSlider.HORIZONTAL,
-//                0, 1, 1);
-//        windowSlider.setPaintLabels(true);
-//        windowSlider.setPaintTicks(true);
-//        windowSlider.setMajorTickSpacing(3);
-//        windowSlider.setMinorTickSpacing(1);
-//        
-//        Dictionary<Integer, Component> windowLabelTable = new Hashtable<Integer, Component>();
-//        windowLabelTable.put(0, new JLabel("OPEN"));
-//        windowLabelTable.put(1, new JLabel("CLOSE"));
-//   
-//        windowSlider.setLabelTable(windowLabelTable);
-//        windowSlider.setEnabled(false);
-//        panel.add(windowSlider);
-//        /**Third Row**/ 
-//        
-//        /**Fourth Row**/ 
-//        JLabel timeLabel = new JLabel("Time " + TIME_INIT);
-//        timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//        panel.add(timeLabel);
-//        
-//        JTextField timeTextField = new JTextField("1200");
-//        timeTextField.setHorizontalAlignment(SwingConstants.CENTER);
-//        timeTextField.addKeyListener(new ClockListener(timeLabel, timeTextField));
-//        panel.add(timeTextField);
-//        
-//        panel.add(new JLabel(""));
-//        panel.add(new JLabel(""));
-//        panel.add(new JLabel(""));
-//        /**Fourth Row**/ 
-//         
-//        experimentLayout.setVgap(5);
-//        experimentLayout.setHgap(5);
-//        
-//        pane.add(panel, BorderLayout.NORTH);
-//        pane.add(new JSeparator(), BorderLayout.CENTER);
-//        String time = "240";
-//        time = new StringBuffer(time).insert(time.length()-2, ":").toString();
-//        System.out.println(time);
+    private void addThermometer(Sensor s) {
+    	JLabel thermometerLabel = new JLabel(((AbstractSensor) s).getProperties().getName());
+        thermometerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        JSlider thermometerSlider = new JSlider(
+        		JSlider.HORIZONTAL,
+                Thermometer.MIN_VALUE, 
+                Thermometer.MAX_VALUE, 
+                ((AbstractSensor) s).getProperties().getValue()
+        );
+        thermometerSlider.setMajorTickSpacing(50);
+        thermometerSlider.setMinorTickSpacing(25);
+        thermometerSlider.setPaintTicks(true);
+        thermometerSlider.setPaintLabels(true);
+        thermometerSlider.addChangeListener(new ThermometerListener(thermometerLabel, s));
+        
+        panel.add(thermometerLabel);
+        panel.add(thermometerSlider);
+	}
+
+	private void addAnemometer(Sensor s) {
+		JLabel anemometerLabel = new JLabel(((AbstractSensor) s).getProperties().getName());
+        anemometerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        JSlider anemometerSlider = new JSlider(
+        		JSlider.HORIZONTAL,
+                Anemometer.MIN_VALUE, 
+                Anemometer.MAX_VALUE, 
+                ((AbstractSensor) s).getProperties().getValue()
+        );
+        anemometerSlider.setMajorTickSpacing(4);
+        anemometerSlider.setMinorTickSpacing(2);
+        anemometerSlider.setPaintTicks(true);
+        anemometerSlider.setPaintLabels(true);
+        anemometerSlider.addChangeListener(new AnemometerListener(anemometerLabel, s));
+        
+        panel.add(anemometerLabel);
+        panel.add(anemometerSlider);
+	}
+
+	private void addClock(Sensor s) {
+		JLabel timeLabel = new JLabel(((AbstractSensor) s).getProperties().getName());
+        timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        JTextField timeTextField = new JTextField(((AbstractSensor) s).getProperties().getValue());
+        timeTextField.setHorizontalAlignment(SwingConstants.CENTER);
+        timeTextField.addKeyListener(new ClockListener(timeLabel, timeTextField, s));
+        
+        panel.add(timeLabel);
+        panel.add(timeTextField);
+	}
+	
+	public void addActor(Actor a) {
+		if(a instanceof AirConditioner) {
+			addAirConditioner(a);
+		} else if(a instanceof Window) {
+			addWindow(a);
+		}
+	}
+
+	private void addAirConditioner(Actor a) {
+		JLabel airConditionerLabel = new JLabel(((AbstractActor) a).getProperties().getName());
+        airConditionerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        JSlider airConditionerSlider = new JSlider(JSlider.HORIZONTAL, 0, 2, 1);
+        airConditionerSlider.setPaintLabels(true);
+        airConditionerSlider.setPaintTicks(true);
+        airConditionerSlider.setMajorTickSpacing(3);
+        airConditionerSlider.setMinorTickSpacing(1);
+        
+        Dictionary<Integer, Component> airConditionerLabelTable = new Hashtable<Integer, Component>();
+        airConditionerLabelTable.put(AirConditioner.OFF, new JLabel("OFF"));
+        airConditionerLabelTable.put(AirConditioner.COOLER_MODE, new JLabel("COOLER"));
+        airConditionerLabelTable.put(AirConditioner.HEATER_MODE, new JLabel("HEATER"));
+   
+        airConditionerSlider.setLabelTable(airConditionerLabelTable);
+        airConditionerSlider.setEnabled(false);
+        
+        panel.add(airConditionerLabel);
+        panel.add(airConditionerSlider);
+	}
+	
+	private void addWindow(Actor a) {
+		JLabel windowLabel = new JLabel("Window");
+        windowLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(windowLabel);
+        
+        JSlider windowSlider = new JSlider(JSlider.HORIZONTAL,
+                0, 1, 1);
+        windowSlider.setPaintLabels(true);
+        windowSlider.setPaintTicks(true);
+        windowSlider.setMajorTickSpacing(3);
+        windowSlider.setMinorTickSpacing(1);
+        
+        Dictionary<Integer, Component> windowLabelTable = new Hashtable<Integer, Component>();
+        windowLabelTable.put(Window.CLOSE, new JLabel("CLOSE"));
+        windowLabelTable.put(Window.OPEN,  new JLabel("OPEN"));
+   
+        windowSlider.setLabelTable(windowLabelTable);
+        windowSlider.setEnabled(false);
+        panel.add(windowSlider);
+	}
+
+	public void addComponentsToPane(final Container pane) {
+        pane.add(panel, BorderLayout.NORTH);
     }
-     //-273<=temp<=100
-    //    0<=anemo<=12
     /**
      * Create the GUI and show it.  For thread safety,
      * this method is invoked from the
      * event dispatch thread.
      */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-    	GUI frame = new GUI("GridLayoutDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Set up the content pane.
-        frame.addComponentsToPane(frame.getContentPane());
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
-     
-    public static void main(String[] args) {
-        /* Use an appropriate Look and Feel */
-        try {
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        /* Turn off metal's use of bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
-         
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
+//    private static void createAndShowGUI() {
+//        //Create and set up the window.
+//    	GUI frame = new GUI("GridLayoutDemo");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        //Set up the content pane.
+//        frame.addComponentsToPane(frame.getContentPane());
+//        //Display the window.
+//        frame.pack();
+//        frame.setVisible(true);
+//    }
+//     
+//    public static void main(String[] args) {
+//        /* Use an appropriate Look and Feel */
+//        try {
+//            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+//            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+//        } catch (UnsupportedLookAndFeelException ex) {
+//            ex.printStackTrace();
+//        } catch (IllegalAccessException ex) {
+//            ex.printStackTrace();
+//        } catch (InstantiationException ex) {
+//            ex.printStackTrace();
+//        } catch (ClassNotFoundException ex) {
+//            ex.printStackTrace();
+//        }
+//        /* Turn off metal's use of bold fonts */
+//        UIManager.put("swing.boldMetal", Boolean.FALSE);
+//         
+//        //Schedule a job for the event dispatch thread:
+//        //creating and showing this application's GUI.
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                createAndShowGUI();
+//            }
+//        });
+//    }
 }
